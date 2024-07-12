477ad2d409b3f711ce14f59637a668aac8117bc1
Nahidul Islam Siam <siamnahidul093@gmail.com>
2024-07-13 01:15:03 +0600

    fff

---
+++ src/Navbars/Navbar1.jsx
@@ -0,0 +1,19 @@
+
+
+const Navbar1 = () => {
+    return (
+     
+           <nav className="p-5 bg-white shadow">
+        <div>
+            <span className="text-2xl font-[Poppins] cursor-pointer">
+                <img className="h-4 inline " src="https://static-00.iconduck.com/assets.00/tailwind-css-icon-1024x615-fdeis5r1.png" alt="" />
+
+tailwind
+            </span>
+        </div>
+           </nav>
+  
+    );
+};
+
+export default Navbar1;
