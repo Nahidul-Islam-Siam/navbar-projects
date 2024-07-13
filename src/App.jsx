62583d0a256bea2a78bcc593af00cf15f1140dac
Nahidul Islam Siam <siamnahidul093@gmail.com>
2024-07-13 13:09:22 +0600

    navbar 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 
 import './App.css'
-import Navbar1 from './Navbars/Navbar1'
+// import Navbar1 from './Navbars/Navbar1'
+import Navbar2 from './Navbars/Navbar2'
 
 function App() {
  
 
   return (
     <>
-   <Navbar1/>
-   
+   {/* <Navbar1/> */}
+   <Navbar2/>
     </>
   )
 }
